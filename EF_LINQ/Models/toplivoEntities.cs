e0dbc34a04e4c01856eff28cecba63c7a8a32f3c
olgasn <olgasn@inbox.ru>
2016-10-01 20:10:37 +0300

    Доработка

--- EF_LINQ/Models/toplivoEntities.cs
+++ EF_LINQ/Models/toplivoEntities.cs
@@ -1,9 +1,6 @@
 namespace EF_LINQ.Models
 {
-    using System;
     using System.Data.Entity;
-    using System.ComponentModel.DataAnnotations.Schema;
-    using System.Linq;
 
     public partial class toplivoEntities : DbContext
     {
--- EF_LINQ/Program.cs
+++ EF_LINQ/Program.cs
@@ -1,15 +1,183 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using EF_LINQ.Models;
+using System;
+using System.Collections;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EF_LINQ
 {
     class Program
     {
+
+        static void Print(string sqltext, IEnumerable items)
+        {
+            Console.WriteLine(sqltext);
+            Console.WriteLine("Записи: ");
+
+            foreach (var item in items)
+
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+
+        static void Insert(toplivoEntities db)
+        {
+            //---------------------
+            // Создать новую емкость
+            Tank tank = new Tank
+            {
+                TankType = "Бочка",
+                TankMaterial = "Дерево",
+                TankVolume = 30,
+                TankWeight = 100
+            };
+            //---------------------
+            // Создать новый вид топлива
+            Fuel fuel = new Fuel
+            {
+                FuelType = "Нитроглицерин",
+                FuelDensity = 3
+
+            };
+
+            // Добавить в DbSet
+            db.Tanks.Add(tank);
+            db.Fuels.Add(fuel);
+            // Сохранить изменения в базе данных
+            db.SaveChanges();
+
+
+        }
+        static void Select(toplivoEntities db)
+        {
+
+            // Определение LINQ запроса 1
+            var queryLINQ1 = from f in db.Operations
+                             join t in db.Fuels
+                             on f.FuelID equals t.FuelID
+                             where (f.Inc_Exp > 0 && f.Date.Value.Year == 2015)
+                             orderby f.FuelID descending
+                             select new
+                             {
+                                 Код_операции = f.OperationID,
+                                 Название_топлива = t.FuelType,
+                                 Приход_Расход = f.Inc_Exp,
+                                 Месяц = f.Date.Value.Month
+                             };
+
+            //то же, используя методы расширений
+            //var queryLINQ1 = db.Operations.Where(f => (f.Inc_Exp > 0 && f.Date.Value.Year == 2015))
+            //.OrderBy(f => f.FuelID)
+            //.Join(db.Fuels, f => f.FuelID, t => t.FuelID, (f, t) => new { f.OperationID, t.FuelType, f.Inc_Exp, f.Date.Value.Month });
+
+            string comment = "1. Результат выполнения запроса на выборку отсортированных записей из двух таблиц, удовлетворяющих заданному условию : \r\n";
+            comment += queryLINQ1.ToString();
+            Print(comment, queryLINQ1.ToList());
+
+            // Определение LINQ запроса 2 
+            var queryLINQ2 = from o in db.Operations
+                             where (o.Inc_Exp > 0 && o.Date.Value.Year == 2015)
+                             group o.Inc_Exp by o.FuelID into gr
+                             select new
+                             {
+                                 Код_топлива = gr.Key,
+                                 Количество_топлива = gr.Sum()
+                             };
+            //то же, используя методы расширений:
+            //var queryLINQ2 = db.Operations.Where(o => ((o.Inc_Exp > (Single?)0) && (o.Date.Value.Year == 2015)))
+            //    .GroupBy(o => o.FuelID, o => o.Inc_Exp)
+            //    .Select(gr => new
+            //    {
+            //        Код_топлива = gr.Key,
+            //        Количества_топлива = gr.Sum()
+            //    }
+            //     );
+
+            comment = "2. Результат выполнения запроса на выборку сгруппированных записей из одной таблицы, удовлетворяющих заданному условию, с выполнением групповой операции суммирования : \r\n";
+            comment += queryLINQ2.ToString();
+            Print(comment, queryLINQ2.ToList());
+
+            // Определение LINQ запроса 3
+            var queryLINQ3 = from t in db.Tanks
+                             orderby t.TankID descending
+                             select new
+                             {
+                                 Название_Емкости = t.TankType,
+                                 Материал_Емкости = t.TankMaterial,
+                                 Объем_Емкости = t.TankVolume,
+                                 Вес = t.TankWeight
+                             };
+
+            comment = "3. Результат выполнения запроса на выборку записей из одной таблицы с выводом определенных полей: \r\n";
+            comment += queryLINQ3.ToString();
+            Print(comment, queryLINQ3.ToList());
+
+
+
+        }
+        static void Delete(toplivoEntities db)
+        {
+            //подлежащие удалению записи в таблице Tanks
+            string nametank = "Бочка";
+            var tank = db.Tanks.Where(c => c.TankType == nametank);
+
+            //подлежащие удалению записи в таблице Fuels
+            string namefuel = "Нитроглицерин";
+            var fuel = db.Fuels
+                .Where(c => c.FuelType == namefuel);
+
+
+            //подлежащие удалению записи в связанной таблице Operations
+            var someOperations = db.Operations
+                .Include("Tank")
+                .Include("Fuel")
+                .Where(o => ((o.Tank.TankType == nametank)) && (o.Fuel.FuelType == namefuel));
+
+            //Удаление нескольких записей в таблице Operations    
+            db.Operations.RemoveRange(someOperations);
+            // сохранить изменения в базе данных
+            db.SaveChanges();
+
+            //Удаление нескольких записей в таблице Tanks и в таблице Fuels
+            db.Tanks.RemoveRange(tank);
+            db.Fuels.RemoveRange(fuel);
+
+            // сохранить изменения в базе данных
+            db.SaveChanges();
+
+
+        }
+        static void Update()
+        {
+
+
+
+
+        }
         static void Main(string[] args)
         {
+            // Создаем экземпляр класса контекста 
+            toplivoEntities db = new toplivoEntities();
+            Console.WriteLine("====== Выборка данных ========");
+            Console.ReadKey();
+            Select(db);
+            Console.WriteLine("====== Будет выполнена вставка данных ========");
+            Console.ReadKey();
+            Insert(db);
+            Console.WriteLine("====== Выборка после вставки ========");
+            Select(db);
+            Console.WriteLine("====== Будет выполнено удаление данных ========");
+            Console.ReadKey();
+            Delete(db);
+            Console.WriteLine("====== Выборка после удаления ========");
+            Select(db);
+
+            db.Dispose();
+
+
         }
     }
 }
